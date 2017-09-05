b112d046d9781ac7ca81cb1be8c6a027381177b2
KimYuRa1 <31638973+KimYuRa1@users.noreply.github.com>
2017-09-05 11:13:47 +0900

    first.java Program
    1

---
+++ first.java
@@ -0,0 +1,13 @@
+
+public class first {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("1.2016301015");
+		System.out.println("2.±Ë¿Ø∂Û");
+		System.out.println("3.010-4814-3118");
+		System.out.println("4.KimYuRa1");
+	}
+
+}
